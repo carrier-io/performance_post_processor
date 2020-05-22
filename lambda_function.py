eef50183ac5aa2c98cf345807e38363c47990212
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-05-22 19:56:30 +0300

    Added exception handling

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,22 @@
 import json
 from perfreporter.post_processor import PostProcessor
-from time import sleep
-from traceback import format_exc
 
 
 def lambda_handler(event=None, context=None):
     try:
-        sleep(10)
         galloper_url, project_id, bucket, prefix, config_file, junit, token = parse_event(event)
         post_processor = PostProcessor(config_file)
         post_processor.distributed_mode_post_processing(galloper_url, project_id, bucket, prefix, junit, token)
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Done')
-        }
-    except:
+
+    except Exception as e:
         return {
             'statusCode': 500,
-            'body': format_exc()
+            'body': json.dumps(str(e))
         }
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Done')
+    }
 
 
 def parse_event(_event):
