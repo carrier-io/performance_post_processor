be81e05de30912648edf69a313cf47ff181da383
Greene1337 <Greeneegl@gmail.com>
2020-07-20 11:12:20 +0000

    Auto Commit

---
+++ perfreporter/post_processor.py
@@ -0,0 +1,210 @@
+from perfreporter.data_manager import DataManager
+from perfreporter.reporter import Reporter
+from perfreporter.jtl_parser import JTLParser
+from perfreporter.junit_reporter import JUnit_reporter
+import requests
+import re
+import shutil
+from os import remove, environ
+from json import JSONDecodeError, loads
+
+
+class PostProcessor:
+
+    def __init__(self, config_file=None):
+        self.config_file = config_file
+
+    def post_processing(self, args, aggregated_errors, galloper_url=None, project_id=None,
+                        junit_report=False, results_bucket=None, prefix=None, token=None, integration=[],
+                        email_recipients=None):
+        if not junit_report:
+            junit_report = environ.get("junit_report")
+        data_manager = DataManager(args)
+        if self.config_file:
+            with open("/tmp/config.yaml", "w") as f:
+                f.write(self.config_file)
+        if not galloper_url:
+            galloper_url = environ.get("galloper_url")
+        if not project_id:
+            project_id = environ.get("project_id")
+        if not token:
+            token = environ.get("token")
+        headers = {'Authorization': f'bearer {token}'} if token else {}
+        reporter = Reporter()
+        rp_service, jira_service = reporter.parse_config_file(args)
+        if not jira_service and "jira" in integration:
+            if galloper_url and token and project_id:
+                secrets_url = f"{galloper_url}/api/v1/secrets/{project_id}/"
+                try:
+                    jira_core_config = loads(requests.get(secrets_url + "jira",
+                                             headers={**headers, 'Content-type': 'application/json'}).json()["secret"])
+                except (AttributeError, JSONDecodeError):
+                    jira_core_config = {}
+                try:
+                    jira_additional_config = loads(requests.get(secrets_url + "jira_perf_api",
+                                                                headers={**headers, 'Content-type': 'application/json'}
+                                                                ).json()["secret"])
+                except (AttributeError, JSONDecodeError):
+                    jira_additional_config = {}
+                jira_service = reporter.get_jira_service(args, jira_core_config, jira_additional_config)
+
+        performance_degradation_rate, missed_threshold_rate = 0, 0
+        compare_with_baseline, compare_with_thresholds = [], []
+        if args['influx_host']:
+            data_manager.write_comparison_data_to_influx()
+            performance_degradation_rate, compare_with_baseline = data_manager.compare_with_baseline()
+            missed_threshold_rate, compare_with_thresholds = data_manager.compare_with_thresholds()
+            reporter.report_performance_degradation(performance_degradation_rate, compare_with_baseline, rp_service,
+                                                    jira_service)
+            reporter.report_missed_thresholds(missed_threshold_rate, compare_with_thresholds, rp_service, jira_service)
+            if junit_report:
+                last_build = data_manager.get_last_build()
+                violations, thresholds = data_manager.get_thresholds(last_build, True)
+                report = JUnit_reporter.create_report(thresholds, prefix)
+                files = {'file': open(report, 'rb')}
+                if project_id:
+                    upload_url = f'{galloper_url}/api/v1/artifacts/{project_id}/{results_bucket}/file'
+                else:
+                    upload_url = f'{galloper_url}/artifacts/{results_bucket}/upload'
+                requests.post(upload_url, allow_redirects=True, files=files, headers=headers)
+                junit_report = None
+            if galloper_url:
+                data = {'build_id': args["build_id"], 'test_name': args["simulation"], 'lg_type': args["influx_db"],
+                        'missed': int(missed_threshold_rate)}
+                if project_id:
+                    url = f'{galloper_url}/api/v1/reports/{project_id}'
+                else:
+                    url = f'{galloper_url}/api/report'
+                r = requests.put(url, json=data, headers={**headers, 'Content-type': 'application/json'})
+                print(r.text)
+        reporter.report_errors(aggregated_errors, rp_service, jira_service, performance_degradation_rate,
+                               compare_with_baseline, missed_threshold_rate, compare_with_thresholds)
+
+        if junit_report:
+            parser = JTLParser()
+            results = parser.parse_jtl()
+            aggregated_requests = results['requests']
+            thresholds = self.calculate_thresholds(results)
+            JUnit_reporter.process_report(aggregated_requests, thresholds)
+        if "email" in integration and email_recipients:
+            if galloper_url and token and project_id:
+                secrets_url = f"{galloper_url}/api/v1/secrets/{project_id}/"
+                try:
+                    email_notification_id = requests.get(secrets_url + "email_notification_id",
+                                                               headers={'Authorization': f'bearer {token}',
+                                                                        'Content-type': 'application/json'}
+                                                               ).json()["secret"]
+                except (AttributeError, JSONDecodeError):
+                    email_notification_id = ""
+                if email_notification_id:
+                    emails = [x.strip() for x in email_recipients.split(",")]
+                    task_url = f"{galloper_url}/api/v1/task/{project_id}/{email_notification_id}"
+                    event = {
+                        "influx_host": args["influx_host"],
+                        "influx_port": args["influx_port"],
+                        "influx_user": args["influx_user"],
+                        "influx_password": args["influx_password"],
+                        "influx_db": args['influx_db'],
+                        "test": args['simulation'],
+                        "user_list": emails,
+                        "notification_type": "api",
+                        "test_type": args["type"],
+                        "users": data_manager.get_user_count()
+                    }
+                    print(event)
+                    res = requests.post(task_url, json=event, headers={'Authorization': f'bearer {token}',
+                                                                        'Content-type': 'application/json'})
+                    print(res.text)
+
+    def distributed_mode_post_processing(self, galloper_url, project_id, results_bucket, prefix, junit=False,
+                                         token=None, integration=[], email_recipients=None):
+        errors = []
+        args = {}
+        # get list of files
+        headers = {'Authorization': f'bearer {token}'} if token else {}
+        if project_id:
+            r = requests.get(f'{galloper_url}/api/v1/artifacts/{project_id}/{results_bucket}',
+                             headers={**headers, 'Content-type': 'application/json'})
+            files = []
+            for each in r.json():
+                if each["name"].startswith(prefix):
+                    files.append(each["name"])
+        else:
+            r = requests.get(f'{galloper_url}/artifacts?q={results_bucket}', headers=headers)
+            pattern = '<a href="/artifacts/{}/({}.+?)"'.format(results_bucket, prefix)
+            files = re.findall(pattern, r.text)
+
+        # download and unpack each file
+        if project_id:
+            bucket_path = f'{galloper_url}/api/v1/artifacts/{project_id}/{results_bucket}'
+        else:
+            bucket_path = f'{galloper_url}/artifacts/{results_bucket}'
+        for file in files:
+            downloaded_file = requests.get(f'{bucket_path}/{file}', headers=headers)
+            with open(f"/tmp/{file}", 'wb') as f:
+                f.write(downloaded_file.content)
+            shutil.unpack_archive(f"/tmp/{file}", "/tmp/" + file.replace(".zip", ""), 'zip')
+            remove(f"/tmp/{file}")
+            with open(f"/tmp/{file}/".replace(".zip", "") + "aggregated_errors.json", "r") as f:
+                errors.append(loads(f.read()))
+            if not args:
+                with open(f"/tmp/{file}/".replace(".zip", "") + "args.json", "r") as f:
+                    args = loads(f.read())
+
+            # delete file from minio
+            if project_id:
+                requests.delete(f'{bucket_path}/file?fname[]={file}', headers=headers)
+            else:
+                requests.get(f'{bucket_path}/{file}/delete', headers=headers)
+
+        # aggregate errors from each load generator
+        aggregated_errors = self.aggregate_errors(errors)
+        self.post_processing(args, aggregated_errors, galloper_url, project_id, junit, results_bucket, prefix, token,
+                             integration, email_recipients)
+
+    @staticmethod
+    def aggregate_errors(test_errors):
+        aggregated_errors = {}
+        for errors in test_errors:
+            for err in errors:
+                if err not in aggregated_errors:
+                    aggregated_errors[err] = errors[err]
+                else:
+                    aggregated_errors[err]['Error count'] = int(aggregated_errors[err]['Error count']) \
+                                                            + int(errors[err]['Error count'])
+
+        return aggregated_errors
+
+    @staticmethod
+    def calculate_thresholds(results):
+        thresholds = []
+        tp_threshold = int(environ.get('tp', 10))
+        rt_threshold = int(environ.get('rt', 500))
+        er_threshold = int(environ.get('er', 5))
+
+        if results['throughput'] < tp_threshold:
+            thresholds.append({"target": "throughput", "scope": "all", "value": results['throughput'],
+                               "threshold": tp_threshold, "status": "FAILED", "metric": "req/s"})
+        else:
+            thresholds.append({"target": "throughput", "scope": "all", "value": results['throughput'],
+                               "threshold": tp_threshold, "status": "PASSED", "metric": "req/s"})
+
+        if results['error_rate'] > er_threshold:
+            thresholds.append({"target": "error_rate", "scope": "all", "value": results['error_rate'],
+                               "threshold": er_threshold, "status": "FAILED", "metric": "%"})
+        else:
+            thresholds.append({"target": "error_rate", "scope": "all", "value": results['error_rate'],
+                               "threshold": er_threshold, "status": "PASSED", "metric": "%"})
+
+        for req in results['requests']:
+
+            if results['requests'][req]['response_time'] > rt_threshold:
+                thresholds.append({"target": "response_time", "scope": results['requests'][req]['request_name'],
+                                   "value": results['requests'][req]['response_time'],
+                                   "threshold": rt_threshold, "status": "FAILED", "metric": "ms"})
+            else:
+                thresholds.append({"target": "response_time", "scope": results['requests'][req]['request_name'],
+                                   "value": results['requests'][req]['response_time'],
+                                   "threshold": rt_threshold, "status": "PASSED", "metric": "ms"})
+
+        return thresholds
---
+++ perfreporter/reporter.py
@@ -0,0 +1,124 @@
+import sys
+import yaml
+
+from perfreporter.report_portal import ReportPortal
+from perfreporter.jira_wrapper import JiraWrapper
+
+
+PATH_TO_CONFIG = "/tmp/config.yaml"
+
+
+class Reporter(object):
+
+    @staticmethod
+    def parse_config_file(args):
+        report_types = []
+        with open(PATH_TO_CONFIG, "rb") as f:
+            config = yaml.load(f.read())
+        if config:
+            report_types = list(config.keys())
+
+        rp_service = None
+        if 'reportportal' in report_types:
+            rp_project = config['reportportal'].get("rp_project_name")
+            rp_url = config['reportportal'].get("rp_host")
+            rp_token = config['reportportal'].get("rp_token")
+            rp_launch_name = config['reportportal'].get("rp_launch_name")
+            check_functional_errors = config['reportportal'].get("check_functional_errors", 'False')
+            check_performance_degradation = config['reportportal'].get("check_performance_degradation", 'False')
+            check_missed_thresholds = config['reportportal'].get("check_missed_thresholds", 'False')
+            performance_degradation_rate = config['reportportal'].get("performance_degradation_rate", 20)
+            missed_thresholds_rate = config['reportportal'].get("missed_thresholds_rate", 50)
+            if not all([rp_project, rp_url, rp_token, rp_launch_name]):
+                print("ReportPortal configuration values missing, proceeding "
+                      "without report portal integration ")
+            else:
+                rp_service = ReportPortal(args, rp_url, rp_token, rp_project, rp_launch_name, check_functional_errors,
+                                          check_performance_degradation, check_missed_thresholds,
+                                          performance_degradation_rate, missed_thresholds_rate)
+                rp_service.my_error_handler(sys.exc_info())
+
+        jira_service = None
+        if 'jira' in report_types:
+            jira_url = config['jira'].get("url", None)
+            jira_user = config['jira'].get("username", None)
+            jira_pwd = config['jira'].get("password", None)
+            jira_project = config['jira'].get("jira_project", None)
+            jira_assignee = config['jira'].get("assignee", None)
+            jira_issue_type = config['jira'].get("issue_type", 'Bug')
+            jira_labels = config['jira'].get("labels", '')
+            jira_watchers = config['jira'].get("watchers", '')
+            jira_epic_key = config['jira'].get("epic_link", None)
+            check_functional_errors = config['jira'].get("check_functional_errors", 'False')
+            check_performance_degradation = config['jira'].get("check_performance_degradation", 'False')
+            check_missed_thresholds = config['jira'].get("check_missed_thresholds", 'False')
+            performance_degradation_rate = config['jira'].get("performance_degradation_rate", 20)
+            missed_thresholds_rate = config['jira'].get("missed_thresholds_rate", 50)
+            if not (jira_url and jira_user and jira_pwd and jira_project and jira_assignee):
+                print("Jira configuration values missing, proceeding without Jira")
+            else:
+                jira_service = JiraWrapper(args, jira_url, jira_user, jira_pwd, jira_project, jira_assignee,
+                                           check_functional_errors, check_performance_degradation,
+                                           check_missed_thresholds, performance_degradation_rate,
+                                           missed_thresholds_rate, jira_issue_type, jira_labels, jira_watchers,
+                                           jira_epic_key)
+
+        return rp_service, jira_service
+
+    def get_jira_service(self, args, jira_config, jira_additional_config):
+        for each in ["jira_url", "jira_login", "jira_password", "jira_project"]:
+            if not jira_config.get(each):
+                print("Jira configuration values missing, proceeding without Jira")
+                return None
+        jira_service = JiraWrapper(args, jira_config["jira_url"], jira_config["jira_login"],
+                                   jira_config["jira_password"], jira_config["jira_project"],
+                                   jira_additional_config.get("assignee", jira_config["jira_login"]),
+                                   jira_additional_config.get("check_functional_errors", False),
+                                   jira_additional_config.get("check_performance_degradation", False),
+                                   jira_additional_config.get("check_missed_thresholds", False),
+                                   jira_additional_config.get("performance_degradation_rate", 20),
+                                   jira_additional_config.get("missed_thresholds_rate", 50),
+                                   jira_config.get("issue_type", "Bug"), jira_additional_config.get("jira_labels", ""),
+                                   jira_additional_config.get("jira_watchers", ""),
+                                   jira_additional_config.get("jira_epic_key", None))
+        return jira_service
+
+    @staticmethod
+    def report_errors(aggregated_errors, rp_service, jira_service, performance_degradation_rate, compare_with_baseline,
+                      missed_threshold_rate, compare_with_thresholds):
+        if rp_service:
+            rp_service.report_test_results(aggregated_errors, performance_degradation_rate, compare_with_baseline,
+                                           missed_threshold_rate, compare_with_thresholds)
+
+        if jira_service and jira_service.check_functional_errors:
+            jira_service.connect()
+            if jira_service.valid:
+                jira_service.report_errors(aggregated_errors)
+            else:
+                print("Failed connection to Jira or project does not exist")
+
+    @staticmethod
+    def report_performance_degradation(performance_degradation_rate, compare_with_baseline, rp_service, jira_service):
+       # if rp_service and rp_service.check_performance_degradation:
+       #    rp_service.report_performance_degradation(performance_degradation_rate, compare_with_baseline)
+
+        if jira_service and jira_service.check_performance_degradation:
+            jira_service.connect()
+            if jira_service.valid:
+                if performance_degradation_rate > jira_service.performance_degradation_rate:
+                    jira_service.report_performance_degradation(performance_degradation_rate, compare_with_baseline)
+            else:
+                print("Failed connection to Jira or project does not exist")
+
+    @staticmethod
+    def report_missed_thresholds(missed_threshold_rate, compare_with_thresholds, rp_service, jira_service):
+        #if rp_service and rp_service.check_missed_thresholds:
+        #   rp_service.report_missed_thresholds(missed_threshold_rate, compare_with_thresholds)
+
+        if jira_service and jira_service.check_missed_thresholds:
+            jira_service.connect()
+            if jira_service.valid:
+                if missed_threshold_rate > jira_service.missed_thresholds_rate:
+                    jira_service.report_missed_thresholds(missed_threshold_rate, compare_with_thresholds)
+            else:
+                print("Failed connection to Jira or project does not exist")
